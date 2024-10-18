4fdaf7d45fd1aec81e2801b968fb5deef0ab432e
Virendra-8x <virendra6x@gmail.com>
2024-10-18 13:15:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('mongodb+srv://taxafe7395:Zn4iVzeiw5ZsGAoA@cluster0.1d2gx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
